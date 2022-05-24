5d3cb167d8c97da7c6493969aef300349a3de4e6
Beatriz <100481568+Beatriz-7@users.noreply.github.com>
2022-05-23 21:43:36 -0300

    Delete PrimeiroPrograma.java

--- PrimeiroPrograma.java
+++
@@ -1,7 +0,0 @@
-public class PrimeiroPrograma {
-        public static void main(String args[]) {
-                int a = 2;
-                int b = 3;
-                System.out.println("Hello world! " + (a+b));      
-        }
-}
